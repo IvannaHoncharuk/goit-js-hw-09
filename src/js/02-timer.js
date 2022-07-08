c9ca106d23c16f6085da4074bb94a3ab3bb17803
Ivanna Honcharuk <Ivannatsyganjuk@gmail.com>
2022-07-08 23:37:40 +0200

    start

---
+++ src/js/02-timer.js
@@ -0,0 +1,93 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+
+const refs = {
+  datePickerInput: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+};
+const options = {
+  enableTime: true,
+  enableSeconds: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  dateFormat: 'd-m-Y H:i:S',
+  onOpen: setCurrentDate,
+  onClose: calculateTimeToTargetDate,
+  onChange: [toggleStartBtn, checkPastDate],
+};
+
+let currentDate = null;
+let timeToTargetDate = null;
+
+refs.startBtn.setAttribute('disabled', 'disabled');
+
+flatpickr(refs.datePickerInput, options);
+
+refs.startBtn.addEventListener('click', startTimer);
+
+function checkPastDate(selectedDate) {
+  if (selectedDate[0] < currentDate) {
+    Notify.failure('Please choose a date in the future');
+  }
+}
+
+function toggleStartBtn(selectedDate) {
+  if (!(selectedDate[0] < currentDate)) {
+    refs.startBtn.removeAttribute('disabled');
+  } else {
+    refs.startBtn.setAttribute('disabled', 'disabled');
+  }
+}
+
+function setCurrentDate() {
+  currentDate = new Date();
+}
+
+function calculateTimeToTargetDate(selectedDate) {
+  timeToTargetDate = selectedDate[0].getTime() - currentDate.getTime();
+}
+
+function startTimer() {
+  refs.startBtn.setAttribute('disabled', 'disabled');
+  const timerID = setInterval(() => {
+    const { days, hours, minutes, seconds } = convertMs(timeToTargetDate);
+
+    document.getElementById('days').innerText = addLeadingZero(days);
+    document.getElementById('hours').innerText = addLeadingZero(hours);
+    document.getElementById('minutes').innerText = addLeadingZero(minutes);
+    document.getElementById('seconds').innerText = addLeadingZero(seconds);
+
+    timeToTargetDate -= 1000;
+
+    if (timeToTargetDate <= 0) {
+      clearInterval(timerID);
+      Report.success('YOU WIN', 'Not to hard, hah? Lets do it again?', 'Okay');
+    }
+  }, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
